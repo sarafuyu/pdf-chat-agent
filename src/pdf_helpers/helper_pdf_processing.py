9fc67672d81a2547d55af5ba2524663d1bb67ccc
Sara Rydell <sara.rydell@ownit.nu>
2024-10-20 21:33:02 +0200

    Update helper_pdf_processing.py

--- src/pdf_helpers/helper_pdf_processing.py
+++ src/pdf_helpers/helper_pdf_processing.py
@@ -2,14 +2,22 @@ import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-def load_and_process_pdf(file):
+def load_and_process_pdf(files):
 
+    documents = []
     # Load documents
-    loader = PyPDFLoader(file)
-    documents = loader.load()
+    for file in files: 
+        loader = PyPDFLoader(file)
+        doc = loader.load()
+        documents.add(doc)
 
     # Split documents
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=0)
-    docs = text_splitter.split_documents(documents)
 
-    return docs
+    docs = []
+    for doc in documents:
+        split = text_splitter.split_documents(doc)
+        docs.add(split)
+
+
+    return split
